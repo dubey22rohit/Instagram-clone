47d46280311e7e3085bfd49445bca929b7d962e4
Rohit Dubey <dubey22rohit@gmail.com>
2020-11-12 18:05:39 +0530

    gitignore

--- server/config/keys.js
+++
@@ -1,4 +0,0 @@
-module.exports = {
-    MongoURI: "mongodb+srv://RohitDubey:98TVYU5JJJ7h5m26@instaclonedb.sr0sq.mongodb.net/<dbname>?retryWrites=true&w=majority",
-    JWT_SECRET: "youcanneverguessthispassword"
-}
