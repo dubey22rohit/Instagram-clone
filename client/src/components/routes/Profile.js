69c6810d103176da5eb60e50106a4a4e7929d2c3
Rohit Dubey <dubey22rohit@gmail.com>
2020-11-15 16:59:03 +0530

    profile page added

--- client/src/components/routes/Profile.js
+++ client/src/components/routes/Profile.js
@@ -1,8 +1,48 @@
-import React from 'react'
+import React from "react";
+import classes from "./Profile.module.css";
+const Profile = () => (
+  <div className={classes.profile}>
+    <div className={classes.bio}>
+      <img
+        src="https://images.unsplash.com/photo-1604936725386-22a475291f50?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=634&q=80"
+        className={classes.bio_pic}></img>
+      <div className={classes.bio_info}>
+        <div className={classes.name_edit_settings}>
+          <div className={classes.person_name}>rohitdubey22</div>
+          <button>Edit profile</button>
+          <div>Settings</div>
+        </div>
+        <div className={classes.friend_count}>
+          <div>
+            <b>16</b> Posts
+          </div>
+          <div>
+            <b>304</b> Followers{" "}
+          </div>
+          <div>
+            <b>313</b> Following{" "}
+          </div>
+        </div>
+        <div className={classes.name_bio}>
+          <div>Rohit Dubey</div>
+          <div>Bio very long no one read</div>
+        </div>
+      </div>
+    </div>
 
-const Profile = ()=>{
-return(
-    <h1>Profile</h1>
-)
-}
-export default Profile
+    <div className = {classes.gallery}>
+    <img src="https://images.unsplash.com/photo-1604936725386-22a475291f50?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=634&q=80"></img>
+    <img src="https://images.unsplash.com/photo-1604936725386-22a475291f50?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=634&q=80"></img>
+    <img src="https://images.unsplash.com/photo-1604936725386-22a475291f50?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=634&q=80"></img>
+    <img src="https://images.unsplash.com/photo-1604936725386-22a475291f50?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=634&q=80"></img>
+    <img src="https://images.unsplash.com/photo-1604936725386-22a475291f50?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=634&q=80"></img>
+    <img src="https://images.unsplash.com/photo-1604936725386-22a475291f50?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=634&q=80"></img>
+    <img src="https://images.unsplash.com/photo-1604936725386-22a475291f50?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=634&q=80"></img>
+    <img src="https://images.unsplash.com/photo-1604936725386-22a475291f50?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=634&q=80"></img>
+    <img src="https://images.unsplash.com/photo-1604936725386-22a475291f50?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=634&q=80"></img>
+    
+    
+    </div>
+  </div>
+);
+export default Profile;
